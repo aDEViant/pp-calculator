975bea0bef8861cc8fc5dad2868a31f78a1b398f
Mohit Sahoo <iamblackwizard2@gmail.com>
2025-10-10 02:02:22 +0530

    target all classes

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.querySelector(".calculatorDisplay");
+let keys = document.querySelectorAll(".key");
+
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const plus = document.querySelector(".plus");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const minus = document.querySelector(".minus");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const multiply = document.querySelector(".multiply");
+const cancel = document.querySelector(".cancel");
+const zero = document.querySelector(".zero");
+const equals = document.querySelector(".equals");
+const divide = document.querySelector(".divide");
+
+// keys.forEach((e) => {
+//     e.addEventListener('click', () => {
+//         display.textContent += e.textContent;
+//     })
+// })
+
+// function splitting(){
+//     let numbers = display.textContent.split(/[+-/*]/);
+//     let operands = display.textContent.match(/[+\-/*]/g);
+// }
